0afd3b457204bae2b359239c66963aea4522b1a9
nafisnihal <nafisnihal12@gmail.com>
2021-01-29 22:20:30 +0600

    First commit

---
+++ Day2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string petName, city, favBand;
+
+            Console.Write("Enter Your Pet Name:");
+            petName = Console.ReadLine();
+
+            Console.Write("Enter Your City:");
+            city = Console.ReadLine();
+
+            Console.Write("Enter Your Favoritr Band:");
+            favBand = Console.ReadLine();
+
+
+            Console.Write("Your password is: " + petName.Substring(0, 3) + city.Substring(0, 3) + favBand.Substring(0, 3));
+
+
+
+        }
+    }
+}
